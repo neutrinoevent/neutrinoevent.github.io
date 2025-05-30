c01b378015144678d509b869f5822e8733cd3ada
neutrinoevent <ranitagain@gmail.com>
2025-05-30 03:57:30 -0400

    Stop tracking _site output

--- _site/assets/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("darkModeToggle");
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    document.body.classList.toggle("light-mode");
-  });
-});
-
